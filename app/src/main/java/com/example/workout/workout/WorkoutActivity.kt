2e450c0555fa44a0cc174ae2a9bbc3c1ee58c61f
Stoonowsky <latara95@gmail.com>
2020-05-06 16:01:25 +0200

    Uzupełniono fragment profilu o ikony oraz funkcjonalność Zastosowano kilka bibliotek zewnętrzych z githuba

---
+++ app/src/main/java/com/example/workout/workout/WorkoutActivity.kt
@@ -0,0 +1,18 @@
+package com.example.workout.workout
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workout.R
+
+class WorkoutActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.workout_activity)
+
+
+
+    }
+
+}
